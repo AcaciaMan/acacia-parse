f56b00a5727d8908a96b15a8925c6f4e1e4ec554
manacacia@gmail.com <manacacia@gmail.com@146f9491-b2ae-59f7-07a2-c522fe6d75da>
2012-11-12 02:09:13 +0000

    Code templates copies from acacia-lex project

---
+++ Parse/src/parser/Parser.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2012 Acacia Man
+ * The program is distributed under the terms of the GNU General Public License
+ * 
+ * This file is part of acacia-lex.
+ *
+ * acacia-lex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * acacia-lex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with acacia-lex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package parser;
+
+/**
+ * 
+ */
+public interface Parser {
+
+    public void setInput(CharSequence input);
+    public CharSequence getInput();
+
+    public Status getStatus();
+
+    public void run();
+
+    public void setInput(java.io.Reader reader) throws java.io.IOException;
+
+    public void setInput(java.io.InputStream is) throws java.io.IOException;
+
+    public void setInput(java.io.File f) throws java.io.FileNotFoundException, java.io.IOException;
+	
+}
